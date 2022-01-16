9905309a802ec61ac2c84a404513a0dbd0dd2f8e
elE3mate <mat.wawski@gmail.com>
2022-01-16 13:27:15 +0000

    Wykonane zadanie E02

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -6,7 +6,24 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int power(int a, int b)
+            {
+                int result = 1;
+                for (int i = b; i > 0; i--)
+                {
+                    result = result * a;
+                }
+                return result;
+
+                if (result < 0)
+                {
+                    Console.WriteLine("Wynik ujemny");
+                }
+
+                
+            }
+
+            Console.WriteLine("Wynik potegowania to: " +power(2, 4));
         }
     }
 }
