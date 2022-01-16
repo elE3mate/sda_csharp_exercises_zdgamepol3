b84238b0aa83172da00e07daa440434a453b78f4
elE3mate <mat.wawski@gmail.com>
2022-01-16 14:06:17 +0000

    Wykonane zadanie E03

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -4,9 +4,28 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        static void ArithmeticSequence(int length, int firstElement, int difference)
+        {
+            int value = firstElement;
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($"{value} ");
+                value += difference;
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj dlugosc");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj pierwszy element");
+            int firstElement = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj roznice ciagu");
+            int difference = Convert.ToInt32(Console.ReadLine());
+
+            ArithmeticSequence(length, firstElement, difference);
         }
     }
 }
