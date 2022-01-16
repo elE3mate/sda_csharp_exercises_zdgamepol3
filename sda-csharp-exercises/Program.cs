30e019c6c5e44c5c51e84eb8db9a1d85bc2a1625
elE3mate <mat.wawski@gmail.com>
2022-01-16 13:08:14 +0000

    Wykonane zadanie E01

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -6,7 +6,16 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int ilosc;
+            int cena;
+
+            ilosc = 3;
+            cena = 5;
+
+            for (int i = 1; i <= ilosc; i++)
+            {
+                Console.WriteLine("Liczba sztuk to: " + i + " a laczny koszt to: " + i*cena + " zlotych");
+            }
         }
     }
 }
