754bcd2c60a05ffc78d17e3ffd23ee71e5416e30
elE3mate <mat.wawski@gmail.com>
2022-01-30 08:52:18 +0000

    Rozwiazanie zadanie 22

---
+++ sda-csharp-exercises/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
